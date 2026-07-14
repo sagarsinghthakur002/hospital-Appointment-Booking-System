7b6982ae5dbbc5346d9ba7be1212376d380e6bd8
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2026-07-14 23:22:50 +0545

    Enhance Navbar styling and structure; update Home component layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,30 @@ import { assets } from "../assets/assets.js";
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="flex justify-between items-center text-sm p-4 mb-5  border-b border-gray-400 ">
 
             <img src={assets.logo} alt="logo" />
 
-            <ul>
+            <ul className="hidden md:flex items-start gap-5 font-medium" >
+
                 <NavLink to="/">
-                    <li>HOME</li>
+                    <li className='py-1 '>HOME</li>
+                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
                 </NavLink>
 
                 <NavLink to="/doctors">
-                    <li> ALL DOCTORS </li>
+                    <li className='py-1 '> ALL DOCTORS </li>
+                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />                
                 </NavLink>
 
                 <NavLink to="/about">
-                    <li>ABOUT</li>
+                    <li className='py-1 '>ABOUT</li>
+                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
                 </NavLink>
 
                 <NavLink to="/contact">
-                    <li>CONTACT</li>
+                    <li className='py-1 '>CONTACT</li>
+                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
                 </NavLink>
 
             </ul>
